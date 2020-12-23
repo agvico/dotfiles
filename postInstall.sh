fbced8f2463bde072fa2167f90cfe152094f19c9
Ángel Miguel García Vico <agvico@ujaen.es>
2020-12-23 13:39:42 +0100

    fixed typo

--- postInstall.sh
+++ postInstall.sh
@@ -115,7 +115,7 @@ spicetify
 spicetify backup apply enable-devtool
 spicetify update
 SPOTIFY_THEME="ShadowCustom"
-picetify config current_theme $SPOTIFY_THEME
+spicetify config current_theme $SPOTIFY_THEME
 spicetify config extensions "queueAll.js|djMode.js|shuffle+.js|fullAppDisplay.js"
 spicetify apply
 
